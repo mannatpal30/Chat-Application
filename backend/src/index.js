c195f0d2323f9da65eea19825bf89e2e2140acbd
mannatpal30 <mannatpal2004@gmail.com>
2025-03-07 18:33:50 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+const app=express();
+
+dotenv.config()
+const PORT = process.env.PORT
+
+app.use(express.json({ limit: "10mb" }));
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+
+}));
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, ()=>{
+    console.log("server in runnning on PORT: "+ PORT);
+    connectDB()
+});
